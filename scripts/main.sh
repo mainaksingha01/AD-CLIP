40eb3c99bd7392f5c1386dce7019a312ddb4d656
Mainak Singha <117339425+mainaksingha01@users.noreply.github.com>
2024-04-20 15:00:51 +0900

    Add files via upload

---
+++ scripts/main.sh
@@ -0,0 +1,40 @@
+cd ..
+
+DATA=/raid/biplab/mainak/data # change your data path here
+MODE=train
+
+DATASET=$1 # dataset name; officehome, visda17, mini_domainnet
+TRAINER=$2 # ADCLIPRN50, ADCLIPB16, ADCLIPL14
+CFG=$3  # config file; rn50, vitB16, vitL14
+SEED=$4
+
+# for SEED in 1 2 3 4 5
+# do
+#     DIR=output/${DATASET}/${MODE}/${TRAINER}/${CFG}/seed_${SEED}
+#     if [ -d "$DIR" ]; then
+#         echo "Results are available in ${DIR}. Skip this job"
+#     else
+#         echo "Run this job and save the output to ${DIR}"
+#         python train.py \
+#         --root ${DATA} \
+#         --seed ${SEED} \
+#         --trainer ${TRAINER} \
+#         --dataset-config-file configs/datasets/${DATASET}.yaml \
+#         --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
+#         --output-dir ${DIR}
+#     fi
+# done
+
+DIR=output/${DATASET}/${MODE}/${TRAINER}/${CFG}/seed_${SEED}
+    if [ -d "$DIR" ]; then
+        echo "Results are available in ${DIR}. Skip this job"
+    else
+        echo "Run this job and save the output to ${DIR}"
+        python train.py \
+        --root ${DATA} \
+        --seed ${SEED} \
+        --trainer ${TRAINER} \
+        --dataset-config-file configs/datasets/${DATASET}.yaml \
+        --config-file configs/trainers/${CFG}.yaml \
+        --output-dir ${DIR}
+    fi
